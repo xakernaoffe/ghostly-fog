e07b61de70e38aecd069d264642ba44cacaa2b97
andrey.makarov <andrey.makarov@ideus.biz>
2017-06-30 01:20:01 +0300

    s

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ghostly-fog/admin/');
+define('HTTP_CATALOG', 'http://ghostly-fog/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://ghostly-fog/admin/');
+define('HTTPS_CATALOG', 'http://ghostly-fog/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OpenServer/domains/ghostly-fog/admin/');
+define('DIR_SYSTEM', 'C:/OpenServer/domains/ghostly-fog/system/');
+define('DIR_LANGUAGE', 'C:/OpenServer/domains/ghostly-fog/admin/language/');
+define('DIR_TEMPLATE', 'C:/OpenServer/domains/ghostly-fog/admin/view/template/');
+define('DIR_CONFIG', 'C:/OpenServer/domains/ghostly-fog/system/config/');
+define('DIR_IMAGE', 'C:/OpenServer/domains/ghostly-fog/image/');
+define('DIR_CACHE', 'C:/OpenServer/domains/ghostly-fog/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OpenServer/domains/ghostly-fog/system/storage/download/');
+define('DIR_LOGS', 'C:/OpenServer/domains/ghostly-fog/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OpenServer/domains/ghostly-fog/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OpenServer/domains/ghostly-fog/system/storage/upload/');
+define('DIR_CATALOG', 'C:/OpenServer/domains/ghostly-fog/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'ghosttyFog');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ghostly-fog/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://ghostly-fog/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OpenServer/domains/ghostly-fog/catalog/');
+define('DIR_SYSTEM', 'C:/OpenServer/domains/ghostly-fog/system/');
+define('DIR_LANGUAGE', 'C:/OpenServer/domains/ghostly-fog/catalog/language/');
+define('DIR_TEMPLATE', 'C:/OpenServer/domains/ghostly-fog/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/OpenServer/domains/ghostly-fog/system/config/');
+define('DIR_IMAGE', 'C:/OpenServer/domains/ghostly-fog/image/');
+define('DIR_CACHE', 'C:/OpenServer/domains/ghostly-fog/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OpenServer/domains/ghostly-fog/system/storage/download/');
+define('DIR_LOGS', 'C:/OpenServer/domains/ghostly-fog/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OpenServer/domains/ghostly-fog/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OpenServer/domains/ghostly-fog/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'ghosttyFog');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
