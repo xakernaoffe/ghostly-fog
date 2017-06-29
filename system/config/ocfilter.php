2d8fb7393b1e4b7aecb04b18bf3bc0bea3b855c9
andrey.makarov <andrey.makarov@ideus.biz>
2017-06-30 01:04:52 +0300

    Удалить Нахер 2,3!!!

--- system/config/ocfilter.php
+++
@@ -1,8 +0,0 @@
-<?php
-// URL Params delimeters
-$_['ocfilter_part_separator'] = ';';
-$_['ocfilter_option_separator'] = ':';
-$_['ocfilter_option_value_separator'] = ',';
-
-// URL GET index
-$_['ocfilter_url_index'] = 'filter_ocfilter';
