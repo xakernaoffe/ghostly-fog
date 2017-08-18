cd69981a760b1c252e7b953a461bc7c49f63007b
andrey.makarov <andrey.makarov@ideus.biz>
2017-08-19 00:26:40 +0300

    simplecheckout install!!

---
+++ catalog/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://gfog-vapes.com.ua/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://gfog-vapes.com.ua/');
+
+// DIR
+define('DIR_APPLICATION','/var/www/valera.rusakov/data/www/gfog-vapes.com.ua/catalog/');
+define('DIR_SYSTEM','/var/www/valera.rusakov/data/www/gfog-vapes.com.ua/system/');
+define('DIR_LANGUAGE','/var/www/valera.rusakov/data/www/gfog-vapes.com.ua/catalog/language/');
+define('DIR_TEMPLATE','/var/www/valera.rusakov/data/www/gfog-vapes.com.ua/catalog/view/theme/');
+define('DIR_CONFIG','/var/www/valera.rusakov/data/www/gfog-vapes.com.ua/system/config/');
+define('DIR_IMAGE','/var/www/valera.rusakov/data/www/gfog-vapes.com.ua/image/');
+define('DIR_CACHE','/var/www/valera.rusakov/data/www/gfog-vapes.com.ua/system/storage/cache/');
+define('DIR_DOWNLOAD','/var/www/valera.rusakov/data/www/gfog-vapes.com.ua/system/storage/download/');
+define('DIR_LOGS','/var/www/valera.rusakov/data/www/gfog-vapes.com.ua/system/storage/logs/');
+define('DIR_MODIFICATION','/var/www/valera.rusakov/data/www/gfog-vapes.com.ua/system/storage/modification/');
+define('DIR_UPLOAD','/var/www/valera.rusakov/data/www/gfog-vapes.com.ua/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'valerRusakovCooL');
+define('DB_PASSWORD', 'h376Zqbp');
+define('DB_DATABASE', 'ghosttyFog');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
